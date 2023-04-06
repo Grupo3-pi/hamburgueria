8f02a182985b64e1e3c045f952c1be5056644385
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-05 21:33:24 -0300

    Criação diretórios html
    Criação diretórios html

--- cardapio/views.py
+++ cardapio/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import  HttpResponse
 
 def cardapio(request):
-    return HttpResponse('Estou em cardapio')
+    return render(request,'cardapio.html')
