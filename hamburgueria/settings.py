66e7e7c8a29aa78eb32c04a1ee13d9beda26ba41
Caio-CGodoy <2109745@aluno.univesp.br>
2023-04-22 14:38:55 -0300

    teste

--- hamburgueria/settings.py
+++ hamburgueria/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-y*2)t66u$(@!3l5&01u(nblayp-q3m0*^)1q1#vhhjvur-tq5!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # ALLOWED_HOSTS = ['127.0.0.1']
